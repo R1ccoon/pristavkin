a430e34a47baa7811e52af4a06c8da7f00201bed
matvey <matveyvlasov364@gmail.com>
2024-05-24 20:59:26 +0300

    fast fix hui

---
+++ pristavkin/users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import CreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+# Create your views here.
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('shop:home')
+    template_name = 'users/signup.html'
